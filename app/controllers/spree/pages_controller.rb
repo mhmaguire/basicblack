d22fe6b5b2635b3a962f6ed15fcb157e1e8ef7b2
Matthew Maguire <mhmaguire0@gmail.com>
2013-06-24 20:26:30 -0700

    added footer pages

--- app/controllers/spree/pages_controller.rb
+++ app/controllers/spree/pages_controller.rb
@@ -2,15 +2,13 @@ class Spree::PagesController < Spree::StoreController
   helper 'spree/base'
   helper 'static_pages'
 
-  def index
+  def self.define_actions(*names)
+  	names.each do |name|
+  		define_method name do 
+  		end
+  	end
   end
 
-  def about
-
-  end
-
-  def contact
-
-  end
+  define_actions :index, :about, :contact, :terms, :policies, :shipping, :sitemap, :search
 
 end
